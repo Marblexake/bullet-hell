5d805e245a1420a8a5abda4078c94f37ca85ced1
Marblexake <qazx29@gmail.com>
2019-02-09 11:54:42 +0800

    spawning bullets

--- myOwnBulletHell/Assets/Scripts/Spawner/SpawnerLevel1.cs
+++ myOwnBulletHell/Assets/Scripts/Spawner/SpawnerLevel1.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class SpawnerLevel1 : MonoBehaviour {
 
     public int nBullets = 10;
-    public GameObject bullet;
+    public GameObject yellowBullet;
+
+    private GameObject spawnedBullet;
 
 	// Use this for initialization
 	void Start () {
-		
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        bullet = Instantiate(bullet);
+        spawnedBullet = Instantiate(yellowBullet);
+
+        spawnedBullet.transform.parent = gameObject.transform;
+
+        //spawnedBullet.transform.parent = null;
+
 
 	}
+
+
+
+
+
 }
