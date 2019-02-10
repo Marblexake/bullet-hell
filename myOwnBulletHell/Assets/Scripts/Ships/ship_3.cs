4bbec2b98ed67ccd215fa063336c952cde71dd26
Marblexake <qazx29@gmail.com>
2019-02-10 13:38:06 +0800

    new pattern 3

--- myOwnBulletHell/Assets/Scripts/Ships/ship_3.cs
+++ myOwnBulletHell/Assets/Scripts/Ships/ship_3.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class ship_3 : MonoBehaviour {
 
+    private float delta = 0.7f;
+    private float zValue;
+    private float ztime;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        //sets the inital rotation of the ship
+        transform.Rotate(0, 0, -50);
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        //interpolates between delta and -delta in a nice curve, just like on the sine graph
+        zValue = delta * Mathf.Sin(ztime * 2f);
+
+        //Debug.Log(Mathf.Sin(ztime)); Delta controls how much the ship rotates
+
+        //applies the rotation 
+        transform.Rotate(0, 0, zValue);
+        ztime += Time.deltaTime;
+
+    }
+
 }
